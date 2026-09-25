4d1c2f29f6daab64d4e1794e39d89dbf0e8243ce
Aias <mdaias29@gmai.com>
2026-09-26 00:35:49 +0600

    added brand section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
 
 const Home = () => {
     return (
         <div>
             <h2>This is Home</h2>
             <Banner></Banner>
+             <Brands></Brands>
         </div>
     );
 };
