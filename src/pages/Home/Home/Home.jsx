3199aa19efdc6f9d5ac57aa721037d6e7e4084b9
Aias <mdaias29@gmai.com>
2026-09-26 01:08:21 +0600

    added reviews section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Brands from '../Brands/Brands';
+import Reviews from '../Reviews/Reviews';
+
+const reviewsPromise = fetch('/reviews.json').then(res => res.json());
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
             <Banner></Banner>
-             <Brands></Brands>
+            <Brands></Brands>
+            <Reviews reviewsPromise={reviewsPromise}></Reviews>
         </div>
     );
 };
