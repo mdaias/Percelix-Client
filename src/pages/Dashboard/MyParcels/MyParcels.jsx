44d738981b5fcbc7d5a1b8fa12c009147f576659
Aias <mdaias29@gmai.com>
2026-09-26 17:15:39 +0600

    load send percel data into Deshboard in table from

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -8,7 +8,7 @@ const MyParcels = () => {
     const axiosSecure = useAxiosSecure();
 
     const { data: parcels = [] } = useQuery({
-        queryKey: ['myParcels', user?.email],
+        queryKey: ['my-parcels', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
@@ -18,6 +18,34 @@ const MyParcels = () => {
     return (
         <div>
             <h2>All of my parcels : {parcels.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Cost</th>
+                            <th>Payment Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            parcels.map((parcel, index) => <tr key={parcel._id}>
+                            <th>{index + 1}</th>
+                            <td>{parcel.parcelName}</td>
+                            <td>{parcel.cost}</td>
+                            <td>Blue</td>
+                            <td>
+                                
+                            </td>
+                        </tr>)
+                        }
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/pages/sendParcel/SendParcel.jsx
+++ src/pages/sendParcel/SendParcel.jsx
@@ -58,6 +58,8 @@ const SendParcel = () => {
 
         console.log('cost', cost);
         
+        data.cost = cost;
+        
         Swal.fire({
             title: "Agree with the Cost?",
             text: `You will be charged ${cost} taka!`,
