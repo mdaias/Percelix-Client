0667661cb5485fd495dbbed0d8365a2f34bf92a4
Aias <mdaias29@gmai.com>
2026-09-26 19:01:45 +0600

    handle delete percel

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -10,6 +10,7 @@ import Rider from "../pages/Rider/Rider";
 import SendParcel from "../pages/sendParcel/SendParcel";
 import DashboardLayout from "../layouts/DashboardLayout";
 import MyParcels from "../pages/Dashboard/MyParcels/MyParcels";
+import Payment from "../pages/Dashboard/Payment/Payment";
 
 export const router = createBrowserRouter([
   {
@@ -62,7 +63,13 @@ export const router = createBrowserRouter([
       {
         path: 'my-parcels', 
         Component: MyParcels
+      },
+      
+      {
+        path: 'payment/:parcelId',
+        Component: Payment
       }
+
     ]
 
   }
