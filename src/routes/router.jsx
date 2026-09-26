23f85166a46492918da302bd51837ee0ac793985
Aias <mdaias29@gmai.com>
2026-09-26 09:09:14 +0600

    create send percel page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -7,7 +7,7 @@ import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Register/Register";
 import PrivateRoute from "./PrivateRoute";
 import Rider from "../pages/Rider/Rider";
-
+import SendParcel from "../pages/sendParcel/SendParcel";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +22,11 @@ export const router = createBrowserRouter([
         {
         path: 'rider',
         element: <PrivateRoute><Rider></Rider></PrivateRoute>
+      },
+      
+      {
+        path: 'send-parcel',
+        element: <PrivateRoute><SendParcel></SendParcel></PrivateRoute>
       },
 
         {
