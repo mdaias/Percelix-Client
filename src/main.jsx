3f35a00f3e54527ad667fa5938340e3852b32be9
Aias <mdaias29@gmai.com>
2026-09-26 16:59:15 +0600

    connected my percel data into deshboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router/dom";
-import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 import './index.css'
 import App from './App.jsx'
 import { router } from './routes/router.jsx';
+import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
